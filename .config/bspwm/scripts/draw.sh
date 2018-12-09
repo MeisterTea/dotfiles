f44b76e9738f1a65d81d05a100ca9d026a9c86ea
MeisterTea <git@pasquierfrancois.com>
2018-12-09 12:40:59 +0100

    draw terms

--- .config/bspwm/scripts/draw.sh
+++ .config/bspwm/scripts/draw.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 read -r X Y W H < <(slop -f "%x %y %w %h" -b 1 -t 0 -q)
-# Width and Height in px need to be converted to columns/rows
-# To get these magic values, make a fullscreen st, and divide your screen width by ${tput cols}, height by ${tput lines} 
-(( W /= 8 ))
-(( H /= 16 ))
-g=${W}x${H}+${X}+${Y}
-bspc rule -a st-256color -o state=floating
-if [ "$1" == "perl6" ]; then
-    st -g $g -e perl6
-else
-    st -g $g
-  fi
+printf "X=$X\nY=$Y" > ~/.config/bspwm/scripts/draw.tmp
+bspc rule -a kitty -o state=floating & kitty -o initial_window_width=$W -o initial_window_height=$H -o remember_window_size=no
