025940805908075c8a1e95e1429f748e1d942fe5
MeisterTea <git@pasquierfrancois.com>
2019-01-22 12:08:15 +0100

    xinitrc: bspwm loading

--- .config/bspwm/scripts/autostart/work.sh
+++ .config/bspwm/scripts/autostart/work.sh
@@ -1,5 +1,4 @@
 ~/.screenlayout/work_dual.sh &
-sleep 1
 feh --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png
 compton -b -d :0 &
 polybar work &
