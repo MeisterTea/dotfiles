cbf7c1ecd33df0d4a7fe6edbfedfd9429fbdb660
MeisterTea <git@pasquierfrancois.com>
2019-01-31 00:13:44 +0100

    zsh: load powerlevel9k from aur install + fix desktops @ home

--- .config/bspwm/scripts/set_desktops.sh
+++ .config/bspwm/scripts/set_desktops.sh
@@ -4,8 +4,11 @@ displays=$(xrandr -q | grep ' connected' | awk '{print $1}')
 echo $displays
 displaysCount=$(echo "$displays" | wc -l)
 if [ "$displaysCount" -eq 1 ]; then
-  #bspc monitor eDP1 -d 壱 弐 参 肆 伍 陸 漆 捌 玖 拾
-  bspc monitor eDP-1 -d 壱 弐 参 肆 伍 陸 漆 捌 玖 拾
+  if [ "$displays" = 'eDP1' ]; then 
+    bspc monitor 'eDP1' -d 壱 弐 参 肆 伍 陸 漆 捌 玖 拾
+  else
+    bspc monitor "$displays" -d 壱 弐 参 肆 伍 陸 漆 捌 玖 拾
+  fi
 else
   for display in $displays
   do
