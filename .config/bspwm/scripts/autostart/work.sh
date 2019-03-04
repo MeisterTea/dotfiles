0215a576db2a6bd09477f7bc93df0217dc436184
MeisterTea <git@pasquierfrancois.com>
2019-03-04 18:12:17 +0100

    work: misc

--- .config/bspwm/scripts/autostart/work.sh
+++ .config/bspwm/scripts/autostart/work.sh
@@ -1,6 +1,6 @@
-#~/.screenlayout/work_dual.sh &
-feh --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png
-#feh --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png
+~/.screenlayout/work_dual.sh &
+#feh --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png
+feh --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png --bg-fill ~/Pictures/Wallpapers/firewatch_orange.png
 dbus-launch compton -CG --config ~/.compton.conf &
 polybar work &
-#polybar work_external &
+polybar work_external &
