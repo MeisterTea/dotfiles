7efedd8116e773d2bbe5bff9af6acc9774883671
MeisterTea <git@pasquierfrancois.com>
2019-04-01 19:55:11 +0200

    rename eslintrc react native + misc adds

--- .eslintrc.react-native.js
+++ .eslintrc.react-native.js
@@ -18,6 +18,7 @@ module.exports = {
     "plugin:react/recommended"
   ],
   'rules': {
+    "comma-spacing": 'error',
     "arrow-spacing": 'error',
     'no-multiple-empty-lines': ["error", { "max": 1, "maxEOF": 1}],
     'space-in-parens': ["error", "never"],
@@ -33,18 +34,35 @@ module.exports = {
     'semi': ['error', 'never'],
     "comma-dangle": [2, "always-multiline"],
     "array-bracket-spacing": ["error", "never"],
+    "react/prefer-stateless-function": "error",
+    "react/jsx-handler-names": "error",
+    "react/jsx-tag-spacing": 'error',
+    "react/no-multi-comp": 'error',
+    "react/no-unused-prop-types": 'error',
+    "react/jsx-indent-props": [2, 2],
+    "react/require-optimization": 'error',
+    "react/no-this-in-sfc": 'error',
+    "react/jsx-sort-default-props": 'error',
+    "react/jsx-closing-tag-location": 'error',
+    "react/no-unused-prop-types": 'error',
+    "react/no-unused-state": 'error',
     "react/prop-types": ["error", { "ignore": ["navigation"] }],
     "react/jsx-one-expression-per-line": [1, { "allow": "single-child" }],
     "react/jsx-indent": ["error", 2],
     "react/jsx-curly-spacing": [2, {"when": "always", "allowMultiline": false}],
     'react/jsx-max-props-per-line': [1, { "maximum": 1 }],
+    "react/jsx-equals-spacing": 'error',
+    "react/sort-prop-types": [1, {
+      "callbacksLast": true,
+      "ignoreCase": true,
+    }],
     "react/jsx-sort-props": [1, {
       "callbacksLast": true,
-      "shorthandLast": true,
       "ignoreCase": true,
-      "reservedFirst": true,
     }],
     "react/jsx-closing-bracket-location": 'error',
+    "react/forbid-prop-types": 'warn',
+    "react/require-default-props": 'warn',
     "react-native/no-unused-styles": 2,
     "react-native/split-platform-components": 2,
     "react-native/no-inline-styles": 2,
@@ -83,4 +101,3 @@ module.exports = {
     }
   }
 }
-
