6734403055647889f485293cd84579b6b16d4e8e
Francois Pasquier <git@pasquierfrancois.com>
2021-07-02 01:48:42 +0200

    nvim: all clean now

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -27,3 +27,11 @@ vim.api.nvim_set_keymap('n', '<Leader>s', [[<Cmd>lua require('spectre').open_vis
 vim.api.nvim_set_keymap('n', '<Leader>sp', [[<Cmd>lua require('spectre').open_file_search()<CR>]], { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<Leader>gs', [[<Cmd>lua require'neogit'.open()<CR>]], { noremap = true, silent = true })
+
+-- nvim-compe
+vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', {expr = true})
+vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', {expr = true})
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
+vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
+vim.api.nvim_set_keymap('i', '<CR>', [[compe#confirm('<CR>')]],
+                        {silent = true, expr = true})
